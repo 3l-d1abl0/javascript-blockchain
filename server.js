9b1ea2fef2c8762dca71186b677a6aef2907e100
sameerb <sameer.barha12@gmail.com>
2018-08-01 03:15:40 +0530

    updated Server Working Config !

--- server.js
+++ server.js
@@ -2,8 +2,40 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const api = require('./api/routes/app');
+
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use('/api', api);
+
+app.use((req, res, next)=>{
+    const error = new Error('Not Found !');
+    error.status = 404;
+    next(error);
+});
+
+//Custom Error Handler
+app.use((error, req, res, next)=>{
+        res.status(error.status || 500);
+        res.json({
+            error:{
+                message: error.message
+            }
+        });
+});
+
+app.use((req, res, next)=>{
+    console.log(req.url);
+    res.status(200).json({
+        message: "Default handler !"
+    });
+});
 
 const port = process.env.PORT || 8080;
 const server = http.createServer(app);
