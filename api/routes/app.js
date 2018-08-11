1d983c0e072fddd779cf2542fcfef43f711e6ad5
sameerb <sameer.barha12@gmail.com>
2018-08-12 03:13:26 +0530

    updated Transaction Endpoint

--- api/routes/app.js
+++ api/routes/app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Blockchain = require('../../blockchain/blockchain');
+
+bcoin = new Blockchain();
 
 router.get('/blockchain', (req, res, next)=>{
-    res.status(200).json({
-        blockchain : "Blockchain-Info"
-    });
+    res.status(200).send(bcoin);
 });
 
-router.post('transaction', (req, res, next)=>{
+router.post('/transaction', (req, res, next)=>{
     /* Create new transaction , new Bloack Index
     */
+   const blockNumber = bcoin.newTransaction(req.body.amount, req.body.sender, req.body.recipient);
    res.status(200).json({
-       success: "Transaction-info"
+       message: `Transaction will be added to ${blockNumber}`
    });
+   
 });
 
 router.get('/mine-block', (req, res, next)=>{
